7f44bc9a49e4b9ed79ffbfb16218c61af5ce91ae
saranya.chittibomma <saranya.chittibomma@prometric.com>
2022-01-12 17:59:27 -0500

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NVRCore.Models;
+using NVRCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NVRCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+
+
+        public CustomerRepository customerRepository;
+        public CustomerController()
+        {
+            this.customerRepository = new CustomerRepository();
+        }
+       
+        [HttpGet]
+        public List<Customer> Get()
+        {
+            return customerRepository.GetCustomers();
+
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post(Customer customer)
+        {
+            HttpResponseMessage responseMessage;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    customerRepository.SaveCustomer(customer);
+                    responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StringContent("Customer " + customer.FirstName + " " + customer.LastName + " added.")
+                    };
+                    return responseMessage;
+                }
+                catch (Exception)
+                {
+                    responseMessage = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                    return responseMessage;
+                }
+            }
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+
+
+        }
+    }
+}
---
+++ Services/CustomerRepository.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NVRCore.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NVRCore.Services
+{
+    public class CustomerRepository
+    {
+        public List<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+            try
+            {
+                string json = String.Concat(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\Services\Data\Customers.json").Where(c => !Char.IsWhiteSpace(c))); 
+                if (json.Length > 2)
+                {
+                    var token = JToken.Parse(json);
+                    if (token is JArray)
+                    {
+                        customers = token.ToObject<List<Customer>>();
+                    }
+                    else if (token is JObject)
+                    {
+                        customers.Add(token.ToObject<Customer>());
+                    }
+                    return customers;
+                }
+                return customers;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void SaveCustomer(Customer customer)
+        {
+            try
+            {
+                List<Customer> customers = new List<Customer>();
+                string newJson = string.Empty;
+                string oldJson = String.Concat(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\Services\Data\Customers.json").Where(c => !Char.IsWhiteSpace(c)));
+                if (oldJson.Length > 2)
+                {
+                    var token = JToken.Parse(oldJson);
+                    if (token is JArray)
+                    {
+                        customers = token.ToObject<List<Customer>>();                        
+                    }
+                    else if (token is JObject)
+                    {
+                        customers.Add(token.ToObject<Customer>());                       
+                    }
+                    customers.Add(customer);
+                    newJson = JsonConvert.SerializeObject(customers);
+                }
+                else
+                {
+                    newJson = JsonConvert.SerializeObject(customer);
+                }
+                File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\Services\Data\Customers.json", newJson);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
